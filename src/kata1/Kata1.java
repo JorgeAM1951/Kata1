912ad0b89122def2b5536e5a03a9f71cf86b2d6b
JorgeAM1951 <100132639+JorgeAM1951@users.noreply.github.com>
2022-09-14 10:00:35 +0100

    Class Person refactored

--- src/kata1/Kata1.java
+++ src/kata1/Kata1.java
@@ -1,12 +1,17 @@
 
 package kata1;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class Kata1 {
 
     public static void main(String[] args) {
-        Person person = new Person("Luis", new Date(95, 10, 18));
+        Calendar date;
+        date = GregorianCalendar.getInstance();
+        date.set(1995, 10, 18);
+        Person person = new Person("Luis", date);
         System.out.println(person.getName() + " tiene " + person.getAge() + " años");
     }
     
